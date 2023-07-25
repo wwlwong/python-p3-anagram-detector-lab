88e23339ce3726a28d045b920e52e8191878033a
William Wong <william.wai.lim.wong@gmail.com>
2023-07-25 10:59:55 -0400

    passes tests

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, anagrams):
+        output = []
+        for word in anagrams:
+            if sorted([x for x in word]) == sorted([y for y in self.word]):
+                output.append(word)
+        
+        return output
